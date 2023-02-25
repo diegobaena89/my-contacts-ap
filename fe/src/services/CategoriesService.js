02e35b7c7e613c4548880a75e1e836ead0e5e3ae
diegobaena89 <diegobf.cello@gmail.com>
2023-02-25 17:15:44 -0300

    feat: Create persistenceCategories data mapper

--- fe/src/services/CategoriesService.js
+++ fe/src/services/CategoriesService.js
@@ -1,12 +1,19 @@
-import HttpClient from './utils/HttpClient';
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+import CategoryMapper from "./mappers/CategoryMapper";
+import HttpClient from "./utils/HttpClient";
 
 class CategoriesService {
   constructor() {
-    this.httpClient = new HttpClient('http://localhost:3333');
+    this.httpClient = new HttpClient("http://localhost:3333");
   }
 
-  listCategories(orderBy = 'asc') {
-    return this.httpClient.get(`/categories?orderBy=${orderBy}`);
+  async listCategories(orderBy = "asc") {
+    const categories = await this.httpClient.get(
+      `/categories?orderBy=${orderBy}`
+    );
+
+    return categories.map(CategoryMapper.toDomain);
   }
 
   getCategoryById(id) {
@@ -14,7 +21,7 @@ class CategoriesService {
   }
 
   createCategory(category) {
-    return this.httpClient.post('/categories', {
+    return this.httpClient.post("/categories", {
       body: category,
     });
   }
