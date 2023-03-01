0fc38d31882616e4358ccad70ee164eb9e936d03
diegobaena89 <diegobf.cello@gmail.com>
2023-03-01 20:30:05 -0300

    refactor: useError custom hook

--- fe/src/hooks/useErrors.js
+++ fe/src/hooks/useErrors.js
@@ -1,30 +1,38 @@
-import { useState } from 'react';
+/* eslint-disable no-unused-expressions */
+/* eslint-disable comma-dangle */
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable quotes */
+import { useState, useCallback } from "react";
 
 export default function useErrors() {
   const [errors, setErrors] = useState([]);
 
-  function setError({ field, message }) {
-    const errorAlreadyExists = errors.find((error) => error.field === field);
+  const setError = useCallback(
+    ({ field, message }) => {
+      const errorAlreadyExists = errors.find((error) => error.field === field);
 
-    if (errorAlreadyExists) {
-      return;
-    }
+      if (errorAlreadyExists) {
+        return;
+      }
 
-    setErrors((prevState) => [
-      ...prevState,
-      { field, message },
-    ]);
-  }
+      setErrors((prevState) => [...prevState, { field, message }]);
+    },
+    [errors]
+  );
 
-  function removeError(fieldName) {
-    setErrors((prevState) => prevState.filter(
-      (error) => error.field !== fieldName,
-    ));
-  }
+  const removeError = useCallback((fieldName) => {
+    setErrors((prevState) =>
+      prevState.filter((error) => error.field !== fieldName)
+    );
+  }, []);
 
-  function getMessageByFieldName(fieldName) {
-    return errors.find((error) => error.field === fieldName)?.message;
-  }
+  const getMessageByFieldName = useCallback(
+    (fieldName) => {
+      errors.find((error) => error.field === fieldName)?.message;
+    },
+    [errors]
+  );
 
   return {
     errors,
